3333d725fabccadd8e6c7aae9dcf30415a99f36e
Mohammad Jamshidi <mohamadjamshidiiii8@gmail.com>
2024-03-05 09:09:19 +0330

    create base file and folders

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      tomato: '#ff6257',
+      }
+    },
+  },
+  plugins: [],
+}
+
