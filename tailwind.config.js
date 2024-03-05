26359ad913ad8f2bf85ce94db427e61d4b1ee979
Mohammad Jamshidi <mohamadjamshidiiii8@gmail.com>
2024-03-05 21:56:41 +0330

    style: create structure and base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
       tomato: '#ff6257',
+      darkSlate: '#242742',
+      charcoal: '#36384e',
+      grey: '#9294a0',
+      },
+      fontFamily: {
+        roboto : ['Roboto'],
       }
     },
   },
